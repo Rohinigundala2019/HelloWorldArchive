76109699e3c8cb8d56c7a25701bfecaa1030fb4c
anveshmuppeda <muppedaanvesh@gmail.com>
2025-03-01 22:05:51 -0600

    Adding Java Source Code

---
+++ languages/java/HelloWorld.java
@@ -0,0 +1,8 @@
+// Language: Java (JDK 17+)
+// Compile: javac HelloWorld.java
+// Run: java HelloWorld
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
