0413ad853aba1b75abdc3c2af9a9784381da2143
anveshmuppeda <muppedaanvesh@gmail.com>
2025-03-01 21:59:35 -0600

    Adding C++ Code

---
+++ languages/cpp/hello-world.cpp
@@ -0,0 +1,11 @@
+// Language: C++ (C++17 Standard)
+// Compile: g++ hello.cpp -o hello
+// Run: ./hello
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
