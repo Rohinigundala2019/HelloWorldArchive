34c54bc9a1fa669958b2558447e7d01dc0358f68
ALLAKONDASANDEEP246 <118200403+ALLAKONDASANDEEP246@users.noreply.github.com>
2025-03-12 18:31:55 +0530

    Added Go language files

---
+++ languages/go/hello.go
@@ -0,0 +1,12 @@
+// Language: Go (Golang)
+// Compile: go build main.go
+// Run: ./main
+// Run without compiling: go run main.go
+
+package main // Defines the main package (entry point of the program)
+
+import "fmt" // Imports the fmt package for formatted I/O
+
+func main() {
+	fmt.Println("Hello, World!") // Prints "Hello, World!" to the console
+}
