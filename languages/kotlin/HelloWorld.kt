dde6d0fad6900f5be5c74a1f071fdee6a6ecfe8a
ALLAKONDASANDEEP246 <allakondasandeep246@gmail.com>
2025-04-25 17:17:12 +0530

    kotlin language file uploaded

---
+++ languages/kotlin/HelloWorld.kt
@@ -0,0 +1,8 @@
+// Language: Kotlin
+// Compile: kotlinc HelloWorld.kt -include-runtime -d HelloWorld.jar
+// Run: java -jar HelloWorld.jar
+// Run with Kotlin CLI: kotlin HelloWorld.kt
+
+fun main() {
+    println("Hello, World!") // Prints "Hello, World!" to the console
+}
